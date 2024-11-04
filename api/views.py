6db32458273eff369e75f35cd6f9554047320393
Ahmed Gorashi <ahmed@agenthost.ai>
2024-11-04 14:55:12 -0800

    Add abilities to retrieve all candidates and interviews for a job

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from django.core import serializers
+from django.db.models import Q
 from api.pagination import DefaultPaginator
 from api.serializer import CandidateSerializer, InterviewSerializer, JobSerializer
 from api.models import Candidate, Interview, Job, InterviewStage
@@ -38,7 +39,7 @@ class InterviewViewSet(viewsets.ModelViewSet):
             raise ValidationError("Candidate is already assigned to this job and is interviewing.")
         return Response({'RESPONSE SUCCESSFUL': True})
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['patch'])
     def assign_candidate_to_interview_stage(self, request, pk=None):
         candidate_id = pk
         interview_id = self.request.data.get("interview_id")
@@ -60,13 +61,44 @@ class InterviewViewSet(viewsets.ModelViewSet):
 
     @action(detail=False)
     def get_all_candidates_per_interview_stage(self, request, pk=None):
-        candidate_fields = ["candidate__name", "candidate__email", "candidate__feedback"]
+        candidate_fields = ["candidate__id", "candidate__name", "candidate__email", "candidate__feedback"]
         candidates_per_interview_stage = {}
         for interview_stage in InterviewStage:
-            candidates_info_per_interview_stage = Interview.objects.filter(interview_stage=InterviewStage(interview_stage).value).values_list(*candidate_fields)
+            candidates_info_per_interview_stage = Interview.objects.filter(interview_stage=InterviewStage(interview_stage).value).values_list("id", *candidate_fields)
             candidates_per_interview_stage[interview_stage] = list(candidates_info_per_interview_stage)
         return Response(candidates_per_interview_stage)
+    
+    @action(detail=True, methods=['get'])
+    def get_all_candidates_for_job(self, request, pk=None):
+        job_id = pk
+        all_interview_ids_per_job = Interview.objects.filter(job_id=job_id).values_list("candidate__id", flat=True)
+        all_candidates_per_job = Candidate.objects.filter(id__in=all_interview_ids_per_job)
+        serializer = CandidateSerializer(all_candidates_per_job, many=True)
+        return Response(serializer.data)
 
+    @action(detail=True, methods=['get'])
+    def get_interview_stages_per_job(self, request, pk=None):
+        job_id = pk
+        format_words = lambda words: words.replace("_", " ").title()
+        interview_stages_per_job_info = {interview_stage.value: {"name": format_words(interview_stage.value), "items": []} 
+                                         for interview_stage in InterviewStage}
+        queryset = super().get_queryset()
+        if job_id != 'all':
+            # all_interviews_per_job = base_queryset & Q(job_id=job_id)
+            queryset = queryset.filter(Q(job_id=job_id))
+        all_interviews_per_job = queryset.values_list(
+            "id", "interview_stage", 
+            "candidate__id", "candidate__name", "candidate__email", "candidate__feedback")
+        for unique_id, interview_and_candidate_info in enumerate(all_interviews_per_job): 
+            interview_id, interview_stage, candidate_id, candidate_name, candidate_email, candidate_feedback = interview_and_candidate_info
+            candidate_info_item = {'id': str(unique_id), 
+                                    'interviewID': str(interview_id),
+                                    'candidateID': str(candidate_id), 
+                                    'candidateName': candidate_name, 
+                                    'candidateEmail': candidate_email,
+                                    'candidateFeedback': candidate_feedback}
+            interview_stages_per_job_info[interview_stage]["items"].append(candidate_info_item)       
+        return Response(interview_stages_per_job_info)    
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
